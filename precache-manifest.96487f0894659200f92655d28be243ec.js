82918b8281c8249c9c8287ca1382229e63f0303f
Civil GMH <civilgmh@gmail.com>
2019-11-04 15:55:52 +0000

    Updates

---
+++ precache-manifest.96487f0894659200f92655d28be243ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53cc130c4854bf5aa3583539d42b65c4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2695419c8b5f93c73244",
+    "url": "/monsters-rolodex/static/css/main.687b5dd3.chunk.css"
+  },
+  {
+    "revision": "ea0fdcf4bbb8d7eec81c",
+    "url": "/monsters-rolodex/static/js/2.ec47da32.chunk.js"
+  },
+  {
+    "revision": "2695419c8b5f93c73244",
+    "url": "/monsters-rolodex/static/js/main.3fc41794.chunk.js"
+  },
+  {
+    "revision": "d71975295fe45a6cbab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.4686dfee.js"
+  }
+]);
