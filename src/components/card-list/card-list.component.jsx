9f24dff0757ffc8462e86835d7fb4326e0eb3609
Civil GMH <civilgmh@gmail.com>
2020-08-04 21:36:10 +0000

    comment out and replace with more explicit notation

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -4,10 +4,19 @@ import { Card } from '../card/card.component';
 
 import './card-list.styles.css';
 
-export const CardList = (props) => (
-    <div className='card-list'>
+export const CardList = props => {
+    console.log(props);
+    return <div className='card-list'>
         {props.monsters.map(monster => (
           <Card key={monster.id} monster={monster} />
         ))}
-    </div>
-);
+    </div>;
+};
+
+// export const CardList = (props) => (
+//     <div className='card-list'>
+//         {props.monsters.map(monster => (
+//           <Card key={monster.id} monster={monster} />
+//         ))}
+//     </div>
+// );
